e3cd907f6f4d8069e90cfcf09346fbfdae2b7050
seyi.idowu <shezi357412@icloud.com>
2025-11-09 00:14:54 +0100

    feat: add plantation creation with wallet connect session validation

---
+++ src/config/appkit.ts
@@ -0,0 +1,30 @@
+import { cookieStorage, createStorage, http } from "@wagmi/core";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+if (!projectId) {
+  throw new Error("Project ID is not defined");
+}
+
+export const networks = [mainnet, arbitrum];
+
+export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+  projectId,
+  networks,
+  transports: networks.reduce(
+    (acc, network) => ({
+      ...acc,
+      [network.id]: http(),
+    }),
+    {}
+  ),
+});
+
+export const wagmiConfig = wagmiAdapter.wagmiConfig;
+
