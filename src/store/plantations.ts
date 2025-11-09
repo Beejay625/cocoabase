7b54c5aff812c8ee9be8ff6d2d72fa171b73a34d
seyi.idowu <shezi357412@icloud.com>
2025-11-09 02:09:36 +0100

    feat: add plantation status updates with wallet connect transaction signing

--- src/store/plantations.ts
+++ src/store/plantations.ts
@@ -241,16 +241,6 @@ const addIntervalToDate = (
   return next;
 };
 
-const differenceInCalendarDays = (start: Date, end: Date) => {
-  const startUtc = Date.UTC(
-    start.getFullYear(),
-    start.getMonth(),
-    start.getDate()
-  );
-  const endUtc = Date.UTC(end.getFullYear(), end.getMonth(), end.getDate());
-  return Math.floor((endUtc - startUtc) / (1000 * 60 * 60 * 24));
-};
-
 type SeedCollaborator = Omit<PlantationCollaborator, "id"> & { id?: string };
 type SeedPlantation = Omit<
   Plantation,
