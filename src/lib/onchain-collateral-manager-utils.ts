bfc70da831a0708fb978f3408099a313279af465
seyi.idowu <shezi357412@icloud.com>
2025-11-10 21:43:25 +0100

    feat: add collateral manager utilities

--- src/lib/onchain-collateral-manager-utils.ts
+++ src/lib/onchain-collateral-manager-utils.ts
@@ -4,61 +4,20 @@ export interface CollateralManager {
   id: bigint;
   collateralToken: Address;
   debtToken: Address;
-  liquidationThreshold: number;
   collateralRatio: number;
-}
-
-export interface CollateralPosition {
-  owner: Address;
-  collateral: bigint;
-  debt: bigint;
-  ratio: number;
+  liquidations: bigint;
 }
 
 export function createCollateralManager(
   collateralToken: Address,
   debtToken: Address,
-  liquidationThreshold: number
+  collateralRatio: number
 ): CollateralManager {
   return {
     id: BigInt(0),
     collateralToken,
     debtToken,
-    liquidationThreshold,
-    collateralRatio: 0,
+    collateralRatio,
+    liquidations: BigInt(0),
   };
-}
-
-export function addCollateral(
-  manager: CollateralManager,
-  owner: Address,
-  collateral: bigint,
-  debt: bigint,
-  collateralPrice: bigint,
-  debtPrice: bigint
-): { manager: CollateralManager; position: CollateralPosition } {
-  const collateralValue = collateral * collateralPrice;
-  const debtValue = debt * debtPrice;
-  const ratio = debtValue > BigInt(0)
-    ? Number((collateralValue * BigInt(10000)) / debtValue) / 100
-    : 0;
-  return {
-    manager: {
-      ...manager,
-      collateralRatio: ratio,
-    },
-    position: {
-      owner,
-      collateral,
-      debt,
-      ratio,
-    },
-  };
-}
-
-export function isLiquidatable(
-  position: CollateralPosition,
-  threshold: number
-): boolean {
-  return position.ratio < threshold;
 }
