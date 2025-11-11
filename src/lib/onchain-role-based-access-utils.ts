be861087c2398eb9f618660639f2ed28eecd9929
seyi.idowu <shezi357412@icloud.com>
2025-11-11 09:05:56 +0100

    feat: add grant, revoke and hasRole functions

--- src/lib/onchain-role-based-access-utils.ts
+++ src/lib/onchain-role-based-access-utils.ts
@@ -14,3 +14,24 @@ export function createRole(name: string): Role {
   };
 }
 
+export function grantRole(role: Role, address: Address): Role {
+  const newMembers = new Set(role.members);
+  newMembers.add(address);
+  return {
+    ...role,
+    members: newMembers,
+  };
+}
+
+export function revokeRole(role: Role, address: Address): Role {
+  const newMembers = new Set(role.members);
+  newMembers.delete(address);
+  return {
+    ...role,
+    members: newMembers,
+  };
+}
+
+export function hasRole(role: Role, address: Address): boolean {
+  return role.members.has(address);
+}
