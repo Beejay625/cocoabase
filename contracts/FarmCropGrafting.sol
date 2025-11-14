0ffdb11948ebb6a8d0b0d4c65d341f683cec213a
seyi.idowu <shezi357412@icloud.com>
2025-11-14 08:57:31 +0100

    feat: Add 5 more onchain features - Crop Transplanting, Livestock Weight, Crop Thinning, Livestock Movement, Crop Grafting

---
+++ contracts/FarmCropGrafting.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title FarmCropGrafting
+ * @dev Onchain system for tracking crop grafting activities
+ */
+contract FarmCropGrafting is Ownable {
+    struct GraftingRecord {
+        uint256 recordId;
+        uint256 plantationId;
+        string rootstockType;
+        string scionType;
+        uint256 graftsPerformed;
+        uint256 graftingDate;
+        address grafter;
+    }
+
+    mapping(uint256 => GraftingRecord) public graftingRecords;
+    mapping(address => uint256[]) public recordsByGrafter;
+    uint256 private _recordIdCounter;
+
+    event GraftingRecorded(
+        uint256 indexed recordId,
+        address indexed grafter,
+        uint256 graftsPerformed
+    );
+
+    constructor() Ownable(msg.sender) {}
+
+    function recordGrafting(
+        uint256 plantationId,
+        string memory rootstockType,
+        string memory scionType,
+        uint256 graftsPerformed
+    ) public returns (uint256) {
+        uint256 recordId = _recordIdCounter++;
+        graftingRecords[recordId] = GraftingRecord({
+            recordId: recordId,
+            plantationId: plantationId,
+            rootstockType: rootstockType,
+            scionType: scionType,
+            graftsPerformed: graftsPerformed,
+            graftingDate: block.timestamp,
+            grafter: msg.sender
+        });
+
+        recordsByGrafter[msg.sender].push(recordId);
+
+        emit GraftingRecorded(recordId, msg.sender, graftsPerformed);
+        return recordId;
+    }
+
+    function getRecord(uint256 recordId) public view returns (GraftingRecord memory) {
+        return graftingRecords[recordId];
+    }
+}
+
---
+++ contracts/FarmLivestockMovement.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title FarmLivestockMovement
+ * @dev Onchain system for tracking livestock movements
+ */
+contract FarmLivestockMovement is Ownable {
+    struct MovementRecord {
+        uint256 recordId;
+        uint256 livestockId;
+        string fromLocation;
+        string toLocation;
+        uint256 movementDate;
+        address recorder;
+    }
+
+    mapping(uint256 => MovementRecord) public movementRecords;
+    mapping(address => uint256[]) public recordsByRecorder;
+    uint256 private _recordIdCounter;
+
+    event MovementRecorded(
+        uint256 indexed recordId,
+        address indexed recorder,
+        string toLocation
+    );
+
+    constructor() Ownable(msg.sender) {}
+
+    function recordMovement(
+        uint256 livestockId,
+        string memory fromLocation,
+        string memory toLocation
+    ) public returns (uint256) {
+        uint256 recordId = _recordIdCounter++;
+        movementRecords[recordId] = MovementRecord({
+            recordId: recordId,
+            livestockId: livestockId,
+            fromLocation: fromLocation,
+            toLocation: toLocation,
+            movementDate: block.timestamp,
+            recorder: msg.sender
+        });
+
+        recordsByRecorder[msg.sender].push(recordId);
+
+        emit MovementRecorded(recordId, msg.sender, toLocation);
+        return recordId;
+    }
+
+    function getRecord(uint256 recordId) public view returns (MovementRecord memory) {
+        return movementRecords[recordId];
+    }
+}
+
