d8fe918e1b866d98a470b0a34aec6b8ddd6ce735
seyi.idowu <shezi357412@icloud.com>
2025-11-16 20:21:24 +0100

    Add supplier reputation and rating system

--- contracts/FarmSupplierReputation.sol
+++ contracts/FarmSupplierReputation.sol
@@ -5,92 +5,58 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title FarmSupplierReputation
- * @dev Onchain supplier reputation and rating system
+ * @dev Supplier reputation and rating system
  */
 contract FarmSupplierReputation is Ownable {
-    struct SupplierReputation {
+    struct SupplierRating {
         address supplier;
-        uint256 reliabilityScore;
-        uint256 qualityScore;
-        uint256 priceScore;
-        uint256 overallRating;
-        uint256 totalOrders;
+        uint256 score;
+        uint256 totalRatings;
         uint256 onTimeDeliveries;
+        uint256 totalDeliveries;
+        uint256 lastUpdated;
     }
 
-    struct SupplierReview {
-        uint256 reviewId;
-        address reviewer;
-        address supplier;
-        uint256 rating;
-        string comment;
-        uint256 timestamp;
-    }
+    mapping(address => SupplierRating) public supplierRatings;
+    mapping(address => mapping(address => bool)) public hasRatedSupplier;
 
-    mapping(address => SupplierReputation) public reputations;
-    mapping(uint256 => SupplierReview) public reviews;
-    mapping(address => uint256[]) public reviewsBySupplier;
-    uint256 private _reviewIdCounter;
-
-    event ReviewSubmitted(
-        uint256 indexed reviewId,
-        address indexed reviewer,
-        address indexed supplier,
-        uint256 rating
-    );
+    event SupplierRated(address indexed supplier, address indexed rater, uint256 score);
+    event RatingUpdated(address indexed supplier, uint256 newScore);
 
     constructor() Ownable(msg.sender) {}
 
-    function submitReview(
-        address supplier,
-        uint256 rating,
-        string memory comment
-    ) public returns (uint256) {
-        require(supplier != address(0), "Invalid supplier address");
-        require(rating >= 1 && rating <= 5, "Rating must be between 1 and 5");
-
-        uint256 reviewId = _reviewIdCounter++;
-        reviews[reviewId] = SupplierReview({
-            reviewId: reviewId,
-            reviewer: msg.sender,
-            supplier: supplier,
-            rating: rating,
-            comment: comment,
-            timestamp: block.timestamp
-        });
-
-        reviewsBySupplier[supplier].push(reviewId);
-
-        if (reputations[supplier].totalOrders == 0) {
-            reputations[supplier] = SupplierReputation({
-                supplier: supplier,
-                reliabilityScore: 0,
-                qualityScore: 0,
-                priceScore: 0,
-                overallRating: 0,
-                totalOrders: 0,
-                onTimeDeliveries: 0
-            });
+    function rateSupplier(address supplier, uint256 score) public {
+        require(score >= 1 && score <= 5, "Invalid score");
+        require(supplier != msg.sender, "Cannot rate yourself");
+        require(!hasRatedSupplier[supplier][msg.sender], "Already rated");
+        
+        hasRatedSupplier[supplier][msg.sender] = true;
+        SupplierRating storage rating = supplierRatings[supplier];
+        if (rating.supplier == address(0)) {
+            rating.supplier = supplier;
+            rating.score = 1000;
         }
-
-        SupplierReputation storage rep = reputations[supplier];
-        rep.totalOrders++;
-        rep.overallRating = ((rep.overallRating * (rep.totalOrders - 1)) + (rating * 20)) / rep.totalOrders;
-
-        emit ReviewSubmitted(reviewId, msg.sender, supplier, rating);
-        return reviewId;
+        
+        rating.totalRatings++;
+        rating.score = ((rating.score * (rating.totalRatings - 1)) + (score * 200)) / rating.totalRatings;
+        rating.lastUpdated = block.timestamp;
+        emit SupplierRated(supplier, msg.sender, score);
+        emit RatingUpdated(supplier, rating.score);
     }
 
-    function recordDelivery(address supplier, bool onTime) public onlyOwner {
-        SupplierReputation storage rep = reputations[supplier];
+    function recordDelivery(address supplier, bool onTime) public {
+        SupplierRating storage rating = supplierRatings[supplier];
+        if (rating.supplier == address(0)) {
+            rating.supplier = supplier;
+            rating.score = 1000;
+        }
+        rating.totalDeliveries++;
         if (onTime) {
-            rep.onTimeDeliveries++;
+            rating.onTimeDeliveries++;
         }
-        rep.reliabilityScore = (rep.onTimeDeliveries * 100) / rep.totalOrders;
     }
 
-    function getReputation(address supplier) public view returns (SupplierReputation memory) {
-        return reputations[supplier];
+    function getSupplierRating(address supplier) public view returns (SupplierRating memory) {
+        return supplierRatings[supplier];
     }
 }
-
