754cb93b615e58045a3d3033a0f10a7ab74cc70f
seyi.idowu <shezi357412@icloud.com>
2025-11-17 20:13:00 +0100

    Add livestock behavior analysis tracking

--- contracts/FarmLivestockBehaviorAnalysis.sol
+++ contracts/FarmLivestockBehaviorAnalysis.sol
@@ -5,25 +5,25 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title FarmLivestockBehaviorAnalysis
- * @dev Onchain behavior analysis and welfare indicators
+ * @dev Onchain livestock behavior observation and analysis
  */
 contract FarmLivestockBehaviorAnalysis is Ownable {
-    struct BehaviorAnalysis {
-        uint256 analysisId;
+    struct BehaviorRecord {
+        uint256 recordId;
         address farmer;
         string livestockId;
         string behaviorType;
-        string observation;
-        uint256 observationDate;
-        string welfareIndicator;
+        string observations;
+        uint256 recordDate;
+        string healthIndicator;
     }
 
-    mapping(uint256 => BehaviorAnalysis) public analyses;
-    mapping(address => uint256[]) public analysesByFarmer;
-    uint256 private _analysisIdCounter;
+    mapping(uint256 => BehaviorRecord) public records;
+    mapping(address => uint256[]) public recordsByFarmer;
+    uint256 private _recordIdCounter;
 
-    event AnalysisRecorded(
-        uint256 indexed analysisId,
+    event BehaviorRecorded(
+        uint256 indexed recordId,
         address indexed farmer,
         string livestockId,
         string behaviorType
@@ -31,30 +31,29 @@ contract FarmLivestockBehaviorAnalysis is Ownable {
 
     constructor() Ownable(msg.sender) {}
 
-    function recordAnalysis(
+    function recordBehavior(
         string memory livestockId,
         string memory behaviorType,
-        string memory observation,
-        string memory welfareIndicator
+        string memory observations,
+        string memory healthIndicator
     ) public returns (uint256) {
-        uint256 analysisId = _analysisIdCounter++;
-        analyses[analysisId] = BehaviorAnalysis({
-            analysisId: analysisId,
+        uint256 recordId = _recordIdCounter++;
+        records[recordId] = BehaviorRecord({
+            recordId: recordId,
             farmer: msg.sender,
             livestockId: livestockId,
             behaviorType: behaviorType,
-            observation: observation,
-            observationDate: block.timestamp,
-            welfareIndicator: welfareIndicator
+            observations: observations,
+            recordDate: block.timestamp,
+            healthIndicator: healthIndicator
         });
 
-        analysesByFarmer[msg.sender].push(analysisId);
-        emit AnalysisRecorded(analysisId, msg.sender, livestockId, behaviorType);
-        return analysisId;
+        recordsByFarmer[msg.sender].push(recordId);
+        emit BehaviorRecorded(recordId, msg.sender, livestockId, behaviorType);
+        return recordId;
     }
 
-    function getAnalysis(uint256 analysisId) public view returns (BehaviorAnalysis memory) {
-        return analyses[analysisId];
+    function getRecord(uint256 recordId) public view returns (BehaviorRecord memory) {
+        return records[recordId];
     }
 }
-
