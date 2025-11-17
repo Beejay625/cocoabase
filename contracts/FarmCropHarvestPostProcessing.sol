b5532b3c7c19baf2bd47c58bdb908f46b9bd71ff
seyi.idowu <shezi357412@icloud.com>
2025-11-17 20:21:53 +0100

    Complete post-processing quality approval

--- contracts/FarmCropHarvestPostProcessing.sol
+++ contracts/FarmCropHarvestPostProcessing.sol
@@ -18,6 +18,7 @@ contract FarmCropHarvestPostProcessing is Ownable {
     }
 
     mapping(uint256 => ProcessingRecord) public records;
+    mapping(address => uint256[]) public recordsByFarmer;
     uint256 private _recordIdCounter;
 
     event ProcessingRecorded(
@@ -27,3 +28,32 @@ contract FarmCropHarvestPostProcessing is Ownable {
     );
 
     constructor() Ownable(msg.sender) {}
+
+    function recordProcessing(
+        string memory harvestId,
+        string memory processingType
+    ) public returns (uint256) {
+        uint256 recordId = _recordIdCounter++;
+        records[recordId] = ProcessingRecord({
+            recordId: recordId,
+            farmer: msg.sender,
+            harvestId: harvestId,
+            processingType: processingType,
+            processingDate: block.timestamp,
+            qualityApproved: false
+        });
+
+        recordsByFarmer[msg.sender].push(recordId);
+        emit ProcessingRecorded(recordId, msg.sender, processingType);
+        return recordId;
+    }
+
+    function approveQuality(uint256 recordId) public {
+        require(records[recordId].farmer == msg.sender, "Not authorized");
+        records[recordId].qualityApproved = true;
+    }
+
+    function getRecord(uint256 recordId) public view returns (ProcessingRecord memory) {
+        return records[recordId];
+    }
+}
